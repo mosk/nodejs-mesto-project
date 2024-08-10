767fb09c1f1596d9c9b5578c031b53bd78276476
mmoskalev <mmoskalev@mobiledep.ru>
2024-08-10 18:50:44 +0300

    *

--- config.ts
+++ config.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'dotenv/config';
+
 interface IAppConfig {
   port: number,
   db: string
@@ -7,8 +10,8 @@ const DEFAULT_PORT = 3000;
 const DEFAULT_DB = 'mongodb://localhost:27017/mestodb';
 
 const getAppConfig = (): IAppConfig => ({
-  port: Number(process.env.PORT) ?? DEFAULT_PORT,
-  db: process.env.DB ?? DEFAULT_DB,
+  port: Number(process.env?.PORT) ?? DEFAULT_PORT,
+  db: process.env?.DB ?? DEFAULT_DB,
 });
 
 export default getAppConfig;
